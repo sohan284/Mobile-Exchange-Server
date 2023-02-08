9da02bb518125b76570d0dafb36784a412ec9d5f
srsohan284 <srsohan284.com>
2023-02-08 15:35:00 +0600

    setup

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p8oa6.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qqahtkg.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -28,39 +28,23 @@ function verifyJWT(req, res, next) {
       next();
     });
   }
-
 async function run(){
     try{
         await client.connect();
-        const productCollection = client.db('as_sunnah').collection('products');
-        const cartCollection = client.db('as_sunnah').collection('cart');
-        const userCollection = client.db('as_sunnah').collection('users');
-        const reviewCollection = client.db('as_sunnah').collection('reviews');
-        const orderCollection = client.db('as_sunnah').collection('orders');
+        const productCollection = client.db('mobile_exchange').collection('products');
+        const cartCollection = client.db('mobile_exchange').collection('cart');
+        const userCollection = client.db('mobile_exchange').collection('users');
+        const orderCollection = client.db('mobile_exchange').collection('orders');
   
   
         // ****************   Product  ***************
         app.get('/product',async(req,res)=>{
-            const page = parseInt(req.query.page);
-            const size = parseInt(req.query.size);
             const query = {};
-            const cursor = productCollection.find(query);
-            let products;
-            if(page || size){
-                 products = await cursor.skip(page * size).limit(size).toArray();
-            }
-            else{
-                 products = await cursor.toArray();
-            }
-           
+            const cursor =   productCollection.find(query)
+            const products =await cursor.toArray();
             res.send(products);
         })
-        app.get('/productCount',async(req,res)=>{
-            const query = {};
-            const cursor = productCollection.find(query);
-            const count = await cursor.count();
-            res.send({count});
-        })
+        
         app.post('/product',async(req,res)=>{
             const newProduct = req.body;
             const result = await productCollection.insertOne(newProduct);
@@ -184,26 +168,7 @@ async function run(){
             }
           
         })
-        //********************** reviews ***********/
-        app.get('/review',async(req,res)=>{ 
-            const reviews = await reviewCollection.find().toArray();
-            res.send(reviews);
-        })
-        app.get('/review/:id',async(req,res)=>{ 
-            const id = req?.params?.id;
-            const query = { id: id };
-            const cursor =reviewCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
-        })
-
-        app.post('/review',async(req,res)=>{
-            const newReview = req.body;
-            const result = await reviewCollection.insertOne(newReview);
-            res.send(result);
-        });
-
-
+       
          //********************** orders ***********/
          app.get('/order',async(req,res)=>{ 
             const orders = await orderCollection.find().toArray();
@@ -235,7 +200,7 @@ async function run(){
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to As Sunnah')
+    res.send('Welcome to Phone exchange')
 })
 
 app.listen(port, () => {
